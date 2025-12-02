0e420334788838132dcd745bf6e6483137bf89ba
comfta <75800727+comfta@users.noreply.github.com>
2025-12-02 01:52:37 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CounterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private int counterValue = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateCounterDisplay();
+        }
+
+        private void OnPlusButtonClicked(object sender, EventArgs e)
+        {
+            counterValue++;
+            UpdateCounterDisplay();
+        }
+
+        private void OnMinusButtonClicked(object sender, EventArgs e)
+        {
+            counterValue--;
+            UpdateCounterDisplay();
+        }
+
+        private void UpdateCounterDisplay()
+        {
+            CounterLabel.Text = counterValue.ToString();
+        }
+    }
+}
